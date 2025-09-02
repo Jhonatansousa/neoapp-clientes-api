50e1ad78e7e3e35ec8484fe9449d2565ddc4ffc3
Jhonatan <jhonatandev@outlook.com>
2025-09-01 23:45:42 -0300

    Initial setup: Spring project structure and database configuration with Docker

---
+++ src/test/java/com/example/neoappclientesapi/NeoappClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neoappclientesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoappClientesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
